c26c9e19372d42b527e10b5ba74bd6d8d7bb3039
Dacarpe03 <d.carmonap97@gmail.com>
2022-11-04 13:01:39 +0100

    Show exploration and explotation with plots

--- SimulatedAnnealing.py
+++ SimulatedAnnealing.py
@@ -36,8 +36,10 @@ def get_function_points(discretized_interval):
 
 def simulated_annealing(interval, 
                         max_iterations, 
-                        initial_temperature, 
-                        neighborhood_radius):
+                        initial_temperature,
+                        alpha,
+                        neighborhood_radius,
+                        animation):
     
     initial_solution = 30
     current_solution = initial_solution
@@ -45,12 +47,28 @@ def simulated_annealing(interval,
     
     best_solution = initial_solution
     
+    accepted_solutions = 0
+    rejected_solutions = 0
+    
     i=0
-    while (current_temperature > 1):
+    while (current_temperature > 0.00001):
         current_neighborhood = get_neighborhood(current_solution, 
                                                 neighborhood_radius)
-        print(current_neighborhood)
         candidate_solution = get_random_point(current_neighborhood)
+        
+        
+        plot_solution(best_solution, 
+                      current_solution, 
+                      candidate_solution,
+                      current_neighborhood)
+        
+        print(f"Iteration {i}:\n"
+              f"    Best solution: {best_solution}\n"
+              f"    Current_solution: {current_solution}\n"
+              f"    Candidate_solution: {candidate_solution}\n"
+              f"    Current_temperature: {current_temperature}\n"
+              f"    Accepted solutions: {accepted_solutions}\n"
+              f"    Rejected solutions: {rejected_solutions}")
         
         if (f(candidate_solution) < f(best_solution)):
             current_solution = candidate_solution
@@ -60,21 +78,25 @@ def simulated_annealing(interval,
             prob_to_accept = get_probability_to_accept(candidate_solution,
                                                        current_solution,
                                                        current_temperature)
+            print(r_value)
+            print(prob_to_accept)
             if (prob_to_accept > r_value):
                 current_solution = candidate_solution
+                accepted_solutions += 1
+            else:
+                rejected_solutions += 1
             
         i+=1
-        current_temperature *= 0.9
-        print(best_solution)
-        plot_solution(best_solution)
+        current_temperature *= alpha
+        
             
 
 def get_probability_to_accept(candidate_solution,
-                              best_solution,
+                              current_solution,
                               temperature):
-    exponential_numerator = f(best_solution) - f(candidate_solution)
+    exponential_numerator = f(current_solution) - f(candidate_solution)
     exponential_denominator = temperature
-    probability = math.exp(-exponential_numerator/exponential_denominator)
+    probability = math.exp(exponential_numerator/exponential_denominator)
     return probability
     
 
@@ -88,28 +110,63 @@ def get_neighborhood(point, radius):
     return [lower_bound, upper_bound] 
 
 
-def plot_solution(solution):
+def plot_solution(best_solution, current_solution, candidate_solution, neighborhood):
     discretized_points = discretize_real_interval(X_DOMAIN, 400)
     function_points = get_function_points(discretized_points)
+    
+    discretized_neighborhood = discretize_real_interval(neighborhood, 25)
+    neighborhood_points =  get_function_points(discretized_neighborhood)
+    
+    plt.scatter(best_solution,
+                f(best_solution), 
+                marker='o', 
+                color='r', 
+                label="Solution")
+    
+    plt.scatter(current_solution,
+                f(current_solution), 
+                marker='s', 
+                color='m', 
+                label="Current solution")
+    
+    plt.scatter(candidate_solution,
+                f(candidate_solution), 
+                marker='^', 
+                color='y', 
+                label="Candidate solution")
+    
     plt.plot(discretized_points, 
              function_points, 
-             'g-')
-    plt.scatter(solution, f(solution), marker='o', color='r', label="Solution")
-
+             'g-',
+             label='f')
+    
+    plt.plot(discretized_neighborhood, 
+             neighborhood_points, 
+             'b-',
+             label='Neighborhood')
+    
     plt.ylabel("f(x)")
     plt.xlabel("x")
     plt.ylim(-1, 2)
     plt.xlim(0, 30)
+    plt.legend()
     plt.show()
     
+    
 def main():
-    initial_temperature = 5000
-    iterations = 1000
-    neighborhood_radius = 4
-    simulated_annealing(X_DOMAIN,
-                        iterations,
-                        initial_temperature,
-                        neighborhood_radius)
+    animation = True
+    if (animation):
+        initial_temperature = 5000
+        iterations = 1000
+        neighborhood_radius = 10
+        temperature_decrease = 0.97
+        simulated_annealing(X_DOMAIN,
+                            iterations,
+                            initial_temperature,
+                            temperature_decrease,
+                            neighborhood_radius,
+                            animation)
+        
     
     
 if __name__ == "__main__":
